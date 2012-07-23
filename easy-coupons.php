c9133a707447ed2d407a5036af8d4953439b0c2e
Mario Alva <magrio.ag@gmail.com>
2012-07-23 18:22:27 +0000

    Changes... i lost myself in code

--- easy-coupons.php
+++ easy-coupons.php
@@ -13,6 +13,7 @@ Version: 0.1
 License: WTFPL :p
 */
 
+
 /**
  * Protection 
  * 
@@ -20,81 +21,94 @@ License: WTFPL :p
  */
 defined('ABSPATH') or die("Cannot access pages directly.");
 
+
 /**
  * Required files
  */
 require 'widgets/categories.widget.php';
 
+
 /**
- * Create post type on WP to the coupons
+ * Create brand new post-type and taxonomy on WP to the coupons
  *
- * @uses Category
- * @uses Tag
+ * @uses Taxonomy
  */
 add_action('init', 'create_coupons_type');
 function create_coupons_type ()
 {
+
 	register_post_type('ecc_coupon',
 		array(
-			'labels' => array(
-				'name' => 'Coupons',
-				'singular_name' => 'Coupon'
+			'labels'                   => array(
+				'name'                 => 'Coupons',
+				'singular_name'        => 'Coupon',
+				'add_new_item'         => 'Add New Coupon',
+				'edit_item'            => 'Edit Coupon',
 			),
-			'public' => true,
-			'show_ui' => true,
-			'capability_type' => 'post',
-			'has_archive' => true,
-			'rewrite' => array('slug' => 'coupon'),
-			'show_in_admin_bar' => true,
-			'taxonomies' => array('category', 'post_tag'),
-			'supports' => array(
-				'title',
-				'editor',
-				'thumbnail',
-				'excerpt',
-				'revisions'
-			)
+			'public'                   => true,
+			'show_ui'                  => true,
+			'capability_type'          => 'post',
+			'has_archive'              => true,
+			'rewrite'                  => array('slug' => 'coupon', 'with_front' => false),
+			'show_in_admin_bar'        => true,
+			'taxonomies'               => array('coupons'),
+			'supports'                 => array('title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'sticky')
 		)
 	);
+
+	$labels = array(
+		'name'                         => 'Categories',
+		'singular_name'                => 'Category',
+		'search_items'                 => 'Search Categories',
+		'popular_items'                => 'Popular Categories',
+		'all_items'                    => 'All Categories',
+		'parent_item'                  => 'Parent Category',
+		'edit_item'                    => 'Edit Category',
+		'update_item'                  => 'Update Category',
+		'add_new_item'                 => 'Add New Category',
+		'new_item_name'                => 'New Category',
+		'separate_items_with_commas'   => 'Separate Categories with commas',
+		'add_or_remove_items'          => 'Add or remove Categories',
+		'choose_from_most_used'        => 'Choose from most used Categories'
+		);
+
+	$args = array(
+		'label'                        => 'Categories',
+		'labels'                       => $labels,
+		'public'                       => true,
+		'hierarchical'                 => true,
+		'show_ui'                      => true,
+		'show_in_nav_menus'            => true,
+		'args'                         => array('orderby' => 'term_order'),
+		'rewrite'                      => array('slug' => 'coupons', 'with_front' => false),
+		'query_var'                    => true
+	);
+
+	register_taxonomy('coupons', 'ecc_coupon', $args);
+
 }
 
-add_filter('pre_get_posts', 'include_coupons_in_categories_and_tags');
-function include_coupons_in_categories_and_tags ($query)
-{
-	if (is_category() || is_tag())
-	{
-		$post_type = get_query_var('post_type');
-		
-		if ($post_type)
-			$post_type = $post_type;
-	
-		else
-			$post_type = array('post','ecc_coupon');
-	
-		$query->set('post_type',$post_type);
-		return $query;
-	}
-}
 
 /**
  * Add the sidebar areas
  */
 register_sidebar(array(
-	'name'=> 'Coupons top',
-	'id' => 'coupons_top',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h2 class="offscreen">',
-	'after_title' => '</h2>',
+	'name'                             => 'Coupons top',
+	'id'                               => 'coupons_top',
+	'before_widget'                    => '<div id="%1$s" class="widget %2$s">',
+	'after_widget'                     => '</div>',
+	'before_title'                     => '<h3 class="widget-title">',
+	'after_title'                      => '</h3>',
 ));
 register_sidebar(array(
-	'name'=> 'Coupons bottom',
-	'id' => 'coupons_bottom',
-	'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	'after_widget' => '</div>',
-	'before_title' => '<h3>',
-	'after_title' => '</h3>',
+	'name'                             => 'Coupons bottom',
+	'id'                               => 'coupons_bottom',
+	'before_widget'                    => '<div id="%1$s" class="widget %2$s">',
+	'after_widget'                     => '</div>',
+	'before_title'                     => '<h3 class="widget-title">',
+	'after_title'                      => '</h3>',
 ));
+
 
 /**
  * Register the widgets
--- widgets/categories.widget.php
+++ widgets/categories.widget.php
@@ -11,23 +11,53 @@ class Categories_Widget extends WP_Widget
 
 	function widget ($args, $instance)
 	{
+
+		$cats = get_categories(array(
+			'type'                     => 'post',
+			'child_of'                 => 0,
+			'parent'                   => '',
+			'orderby'                  => 'count',
+			'order'                    => 'ASC',
+			'hide_empty'               => true,
+			'hierarchical'             => 1,
+			'exclude'                  => '',
+			'include'                  => '',
+			'number'                   => $instance['cats_to_display'],
+			'taxonomy'                 => 'coupons',
+			'pad_counts'               => false
+		));
+		// $cats = get_terms('coupons');
+
+		if ( ! $cats || empty($cats))
+			return;
+
 		?>
+
 		<!-- BEGIN WIDGET -->
-		<div class="widget">
-			<h3 class="widget-title"><?php echo $instance['title'] ?></h3>
-			<ul>
+		<?php echo $args['before_widget'] ?>
+			<?php echo $args['before_title'] ?><?php echo $instance['title'] ?><?php echo $args['after_title'] ?>
+			<ul id="list-coupon_category">
+				
+				<?php foreach ($cats as $cat): ?>
+				
+				<li id="ecc_category_<?php echo $cat->term_taxonomy_id ?>" class="category category-<?php echo $cat->slug ?>">
+					<a href="<?php echo get_term_link($cat->slug, 'coupons') ?>"><?php echo $cat->name ?></a>
+				</li>
+				
+				<?php endforeach ?>
 
 			</ul>
-		</div>
+		<?php echo $args['after_widget'] ?>
 		<!-- END WIDGET -->
+
 		<?php
 	}
 
 	function update ($new_instance, $old_instance)
 	{
 
-		if ( ! isset($new_instance['cats_to_show_up']) || ! is_numeric($new_instance['cats_to_show_up']))
-			$new_instance['cats_to_show_up'] = 10;
+		if ( ! isset($new_instance['cats_to_display']) || ! is_numeric($new_instance['cats_to_display']))
+			$new_instance['cats_to_display'] = 10;
 
 		if ( ! isset($new_instance['title']) || empty($new_instance['title']))
 			$new_instance['title'] = 'popular <span>categories</span>';
@@ -38,21 +68,21 @@ class Categories_Widget extends WP_Widget
 	function form ($instance)
 	{
 
-		if ( ! isset($instance['cats_to_show_up']) || ! is_numeric($instance['cats_to_show_up']))
-			$instance['cats_to_show_up'] = 10;
+		if ( ! isset($instance['cats_to_display']) || ! is_numeric($instance['cats_to_display']))
+			$instance['cats_to_display'] = 10;
 
 		if ( ! isset($instance['title']) || empty($instance['title']))
 			$instance['title'] = 'popular <span>categories</span>';
 
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>">Title: </label>
-				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+			<label for="<?php echo $this->get_field_id('title') ?>">Title: </label>
+				<input class="widefat" id="<?php echo $this->get_field_id('title') ?>" name="<?php echo $this->get_field_name('title') ?>" type="text" value="<?php echo $instance['title'] ?>" />
 			</label>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('cats_to_show_up'); ?>">Number of categories to show: </label>
-				<input id="<?php echo $this->get_field_id('cats_to_show_up'); ?>" name="<?php echo $this->get_field_name('cats_to_show_up'); ?>" type="text" size="3" value="<?php echo $instance['cats_to_show_up']; ?>" />
+			<label for="<?php echo $this->get_field_id('cats_to_display') ?>">Number of categories to show: </label>
+				<input id="<?php echo $this->get_field_id('cats_to_display') ?>" name="<?php echo $this->get_field_name('cats_to_display'); ?>" type="text" size="3" value="<?php echo $instance['cats_to_display'] ?>" />
 			</label>
 		</p>
 		<?php
